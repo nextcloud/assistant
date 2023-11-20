f2b944b39ca266fc67e290167acf13f4bbe0c215
Julien Veyssier <julien-nc@posteo.net>
2023-11-20 10:35:31 +0100

    adjust free prompt submit button label for sync flow
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/AssistantForm.vue
+++ src/components/AssistantForm.vue
@@ -183,7 +183,11 @@ export default {
 					: this.selectedTaskType.name
 		},
 		syncSubmitButtonLabel() {
-			return this.myOutput.trim() ? t('assistant', 'Try again') : this.selectedTaskType.name
+			return this.myOutput.trim()
+				? t('assistant', 'Try again')
+				: this.selectedTaskType.id === 'OCP\\TextProcessing\\FreePromptTaskType'
+					? t('assistant', 'Send request')
+					: this.selectedTaskType.name
 		},
 		showCopy() {
 			return !!this.myOutput.trim()
