09605fc1b33b7ef556a54d69402a8e6e0e3d9185
Julien Veyssier <julien-nc@posteo.net>
2024-02-02 11:39:03 +0100

    simplify resetting result input since richContentEditable was apparently fixed
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/AssistantPlainTextResult.vue
+++ src/components/AssistantPlainTextResult.vue
@@ -40,7 +40,7 @@
 				<NcButton :disabled="output === myOutput"
 					type="secondary"
 					:title="t('assistant', 'Reset the output value to the originally generated one')"
-					@click="delayedReset">
+					@click="onReset">
 					{{ t('assistant', 'Reset') }}
 				</NcButton>
 			</div>
@@ -125,21 +125,6 @@ export default {
 		},
 		onReset() {
 			this.myOutput = this.output
-		},
-
-		delayedReset() {
-			// This is a hack to ensure the text box is updated
-			// when we reset the text since removing newlines or spaces
-			// from the end of the text does not trigger an update.
-
-			// Delete any trailing newlines
-			this.myOutput = this.myOutput.replace(/\n+$/, '')
-			this.myOutput += '.'
-
-			// Let the ui refresh before resetting the text
-			setTimeout(() => {
-				this.onReset()
-			}, 0)
 		},
 	},
 }
