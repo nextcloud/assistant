b185623214f7d3727a5cb0be0c4cd3b3fd9569d2
Julien Veyssier <julien-nc@posteo.net>
2024-02-19 14:58:35 +0100

    fix code style
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- tests/stubs/oc_transcription.php
+++ tests/stubs/oc_transcription.php
@@ -4,10 +4,8 @@ namespace OC\SpeechToText {
 
 	use OCP\BackgroundJob\QueuedJob;
 
-	class TranscriptionJob extends QueuedJob
-	{
-		protected function run($argument)
-		{
+	class TranscriptionJob extends QueuedJob {
+		protected function run($argument) {
 		}
 	}
 }
