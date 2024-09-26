87f7b4bbb188214e946279f22e023a106240dbca
Julien Veyssier <julien-nc@posteo.net>
2024-09-26 16:59:48 +0200

    add back buttons in running and scheduled empty contents
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/components/RunningEmptyContent.vue
+++ src/components/RunningEmptyContent.vue
@@ -3,7 +3,7 @@
 		:name="t('assistant', 'Getting results…')"
 		:description="description">
 		<template #action>
-			<div class="actions">
+			<div class="running-actions">
 				<div v-if="progress !== null"
 					class="progress">
 					<span>{{ formattedProgress }} %</span>
@@ -12,11 +12,24 @@
 				</div>
 				<NcButton
 					@click="$emit('background-notify')">
-					{{ t('assistant', 'Run in the background and get notified') }}
+					<template #icon>
+						<ProgressClockIcon />
+					</template>
+					{{ t('assistant', 'Run task in the background and get notified') }}
+				</NcButton>
+				<NcButton
+					@click="$emit('back')">
+					<template #icon>
+						<ArrowLeftIcon />
+					</template>
+					{{ t('assistant', 'Back to the Assistant') }}
 				</NcButton>
 				<NcButton
 					@click="$emit('cancel')">
-					{{ t('assistant', 'Cancel') }}
+					<template #icon>
+						<CloseIcon />
+					</template>
+					{{ t('assistant', 'Cancel task') }}
 				</NcButton>
 			</div>
 		</template>
@@ -27,6 +40,10 @@
 </template>
 
 <script>
+import ArrowLeftIcon from 'vue-material-design-icons/ArrowLeft.vue'
+import CloseIcon from 'vue-material-design-icons/Close.vue'
+import ProgressClockIcon from 'vue-material-design-icons/ProgressClock.vue'
+
 import NcLoadingIcon from '@nextcloud/vue/dist/Components/NcLoadingIcon.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
 import NcProgressBar from '@nextcloud/vue/dist/Components/NcProgressBar.js'
@@ -40,6 +57,9 @@ export default {
 		NcEmptyContent,
 		NcLoadingIcon,
 		NcProgressBar,
+		ArrowLeftIcon,
+		CloseIcon,
+		ProgressClockIcon,
 	},
 
 	props: {
@@ -56,6 +76,7 @@ export default {
 	emits: [
 		'cancel',
 		'background-notify',
+		'back',
 	],
 
 	data() {
@@ -81,9 +102,10 @@ export default {
 </script>
 
 <style lang="scss">
-.actions {
+.running-actions {
 	display: flex;
 	flex-direction: column;
+	align-items: center;
 	gap: 12px;
 
 	.progress {
