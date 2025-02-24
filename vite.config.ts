687a475ddc025348f881097f10cec465e972ec35
Julien Veyssier <julien-nc@posteo.net>
2025-02-24 13:53:17 +0100

    remove a vite warning
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,9 @@ export default createAppConfig({
 			},
 		},
 		plugins: [eslint(), stylelint()],
+		build: {
+			cssCodeSplit: true,
+		},
 	},
 	inlineCSS: { relativeCSSInjection: true },
 	minify: isProduction,
