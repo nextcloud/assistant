8701f0bb6ab093bfeb0ac676e988a546076b67a4
Lukas Schaefer <lukas@lschaefer.xyz>
2025-07-14 14:51:07 -0400

    feat: Add categories with icons
    Signed-off-by: Lukas Schaefer <lukas@lschaefer.xyz>

--- src/components/TaskTypeSelect.vue
+++ src/components/TaskTypeSelect.vue
@@ -5,43 +5,49 @@
 <template>
 	<div ref="taskTypeSelect"
 		class="task-type-select">
-		<NcButton v-for="(t, i) in buttonTypes"
-			:key="i + t.id"
-			:variant="getButtonType(t)"
-			:title="t.description"
-			@click="onTaskSelected(t)">
-			{{ t.name }}
-		</NcButton>
-		<NcActions v-if="!onlyInline && actionTypes.length > 0"
+		<NcActions v-for="variants in buttonTypes"
+			:key="variants.id"
+			:menu-name="variants.text"
 			:force-menu="true"
-			:container="$refs.taskTypeSelect">
-			<NcActionButton v-for="(t, i) in actionTypes"
-				:key="i + t.id"
-				class="no-icon-action"
-				:aria-label="t.name"
+			:container="$refs.taskTypeSelect"
+			:primary="selectedCategory(variants)"
+			@click="onMenuCategorySelected(variants.id, variants.tasks)">
+			<NcActionButton v-for="t in variants.tasks"
+				:key="t.id"
+				:disabled="selectedTask(t)"
+				:title="t.description"
 				:close-after-click="true"
-				@click="onMenuTaskSelected(t)">
+				@click="onTaskSelected(t)">
 				<template #icon>
 					<div style="width: 16px" />
 				</template>
 				{{ t.name }}
 			</NcActionButton>
+			<template #icon>
+				<component :is="variants.icon" />
+			</template>
 		</NcActions>
 	</div>
 </template>
 
 <script>
-import NcButton from '@nextcloud/vue/components/NcButton'
 import NcActions from '@nextcloud/vue/components/NcActions'
 import NcActionButton from '@nextcloud/vue/components/NcActionButton'
+import MessageOutlineIcon from 'vue-material-design-icons/MessageOutline.vue'
+import DotsHorizontalIcon from 'vue-material-design-icons/DotsHorizontal.vue'
+import TextLongIcon from 'vue-material-design-icons/TextLong.vue'
+import ImageOutlineIcon from 'vue-material-design-icons/ImageOutline.vue'
+import WebIcon from 'vue-material-design-icons/Web.vue'
+import WaveformIcon from 'vue-material-design-icons/Waveform.vue'
+import FileIcon from 'vue-material-design-icons/File.vue'
 
 export default {
 	name: 'TaskTypeSelect',
 
 	components: {
-		NcButton,
 		NcActions,
 		NcActionButton,
+		MessageOutlineIcon,
 	},
 
 	props: {
@@ -52,14 +58,6 @@ export default {
 		options: {
 			type: Array,
 			required: true,
-		},
-		/**
-		 * Number of inline elements
-		 * All elements are inline if this prop is null
-		 */
-		inline: {
-			type: [Number, null],
-			default: null,
 		},
 	},
 
@@ -74,21 +72,37 @@ export default {
 	},
 
 	computed: {
-		onlyInline() {
-			return this.inline === null
-		},
 		buttonTypes() {
-			if (this.onlyInline) {
-				return this.options
+			const taskTypes = {}
+			for (const task of this.options) {
+				const type = this.getTaskCategory(task.id)
+				if (!taskTypes[type]) {
+					taskTypes[type] = []
+				}
+				taskTypes[type].push(task)
 			}
-			// extra button replaces the last one
-			if (this.extraButtonType !== null) {
-				const types = this.options.slice(0, this.inline - 1)
-				types.push(this.extraButtonType)
-				return types
-			} else {
-				return this.options.slice(0, this.inline)
+			const result = []
+			for (const part of Object.entries(taskTypes)) {
+				if (part[0] === 'other') {
+					continue
+				}
+				result.push({
+					id: part[0],
+					text: this.getTextForCategory(part[0]),
+					icon: this.getCategoryIcon(part[0]),
+					tasks: part[1],
+				})
 			}
+			// Ensure the "other" category is always last
+			if (taskTypes.other) {
+				result.push({
+					id: 'other',
+					text: this.getTextForCategory('other'),
+					icon: this.getCategoryIcon('other'),
+					tasks: taskTypes.other,
+				})
+			}
+			return result
 		},
 		actionTypes() {
 			if (this.extraButtonType !== null) {
@@ -106,38 +120,75 @@ export default {
 		},
 	},
 
-	watch: {
-		options() {
-			this.moveSelectedIfInMenu()
-		},
-	},
-
 	mounted() {
-		this.moveSelectedIfInMenu()
 	},
 
 	methods: {
-		moveSelectedIfInMenu() {
-			if (this.onlyInline) {
-				return
-			}
-			// if the initially selected value is in the dropdown, get it out
-			const selectedAction = this.actionTypes.find(a => a.id === this.modelValue)
-			if (this.actionTypes.find(a => a.id === this.modelValue)) {
-				this.extraButtonType = selectedAction
-			}
-		},
-		getButtonType(taskType) {
+		selectedTask(taskType) {
 			return taskType.id === this.modelValue
-				? 'primary'
-				: 'secondary'
+		},
+		selectedCategory(category) {
+			return category.id === this.getTaskCategory(this.modelValue || '')
 		},
 		onTaskSelected(taskType) {
 			this.$emit('update:model-value', taskType.id)
 		},
-		onMenuTaskSelected(taskType) {
-			this.extraButtonType = taskType
-			this.onTaskSelected(taskType)
+		onMenuCategorySelected(category, tasks) {
+			if (tasks.length === 1) {
+				this.onTaskSelected(tasks[0])
+			}
+		},
+		getTaskCategory(id) {
+			if (id.startsWith('chatty')) {
+				return 'chat'
+			} else if (id.includes('translate')) {
+				return 'translate'
+			} else if (id.startsWith('richdocuments')) {
+				return 'generate'
+			} else if (id.includes('image')) {
+				return 'image'
+			} else if (id.includes('audio') || id.includes('speech')) {
+				return 'audio'
+			} else if (id.includes('text')) {
+				return 'text'
+			}
+			return 'other'
+		},
+		getTextForCategory(category) {
+			switch (category) {
+			case 'chat':
+				return t('assistant', 'Chat with AI')
+			case 'text':
+				return t('assistant', 'Work with text')
+			case 'image':
+				return t('assistant', 'Work with images')
+			case 'translate':
+				return t('assistant', 'Translate')
+			case 'audio':
+				return t('assistant', 'Work with audio')
+			case 'generate':
+				return t('assistant', 'Generate file')
+			default:
+				return t('assistant', 'Other')
+			}
+		},
+		getCategoryIcon(category) {
+			switch (category) {
+			case 'chat':
+				return MessageOutlineIcon
+			case 'text':
+				return TextLongIcon
+			case 'image':
+				return ImageOutlineIcon
+			case 'translate':
+				return WebIcon
+			case 'audio':
+				return WaveformIcon
+			case 'generate':
+				return FileIcon
+			default:
+				return DotsHorizontalIcon
+			}
 		},
 	},
 }
