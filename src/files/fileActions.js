46b1fb471498042580acf6b21da5f977ecbca76a
Julien Veyssier <julien-nc@posteo.net>
2025-07-15 18:13:23 +0200

    feat(file-actions): add 3 file actions to summarize, tts and stt
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

---
+++ src/files/fileActions.js
@@ -0,0 +1,164 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { registerFileAction, Permission, FileAction, FileType } from '@nextcloud/files'
+import { loadState } from '@nextcloud/initial-state'
+
+import CreationSvgIcon from '@mdi/svg/svg/creation.svg?raw'
+import TextBoxOutlineSvgIcon from '@mdi/svg/svg/text-box-outline.svg?raw'
+import AccountVoiceSvgIcon from '@mdi/svg/svg/account-voice.svg?raw'
+import MicrophoneMessageSvgIcon from '@mdi/svg/svg/microphone-message.svg?raw'
+
+const actionIgnoreLists = [
+	'trashbin',
+	'files.public',
+]
+
+function registerGroupAction() {
+	const groupAction = new FileAction({
+		id: 'assistant-group',
+		displayName: (nodes) => {
+			return t('assistant', 'Assistant')
+		},
+		enabled(nodes, view) {
+			return !actionIgnoreLists.includes(view.id)
+				&& nodes.length === 1
+				&& !nodes.some(({ permissions }) => (permissions & Permission.READ) === 0)
+				&& nodes.every(({ type }) => type === FileType.File)
+			// && nodes.every(({ mime }) => ['text/plain', 'text/markdown'].includes(mime))
+		},
+		iconSvgInline: () => CreationSvgIcon,
+		order: 0,
+		async exec() {
+			return null
+		},
+	})
+	registerFileAction(groupAction)
+}
+
+function registerSummarizeAction() {
+	const summarizeAction = new FileAction({
+		id: 'assistant-summarize',
+		parent: 'assistant-group',
+		displayName: (nodes) => {
+			return t('approval', 'Summarize')
+		},
+		enabled(nodes, view) {
+			return !actionIgnoreLists.includes(view.id)
+				&& nodes.length === 1
+				&& !nodes.some(({ permissions }) => (permissions & Permission.READ) === 0)
+				&& nodes.every(({ type }) => type === FileType.File)
+				&& nodes.every(({ mime }) => ['text/plain', 'text/markdown'].includes(mime))
+		},
+		iconSvgInline: () => TextBoxOutlineSvgIcon,
+		order: 0,
+		async exec(node) {
+			const { default: axios } = await import('@nextcloud/axios')
+			const { generateOcsUrl } = await import('@nextcloud/router')
+			const { showError, showSuccess } = await import('@nextcloud/dialogs')
+			const url = generateOcsUrl('/apps/assistant/api/v1/file-action/{fileId}/core:text2text:summary', { fileId: node.fileid })
+			try {
+				const response = await axios.post(url)
+				console.debug('taskId', response.data.ocs.data.taskId)
+				showSuccess(t('assistant', 'The result file will be created soon'))
+			} catch (error) {
+				console.error(error)
+				showError(t('assistant', 'Failed to launch the AI file action'))
+			}
+			return null
+		},
+	})
+	registerFileAction(summarizeAction)
+}
+
+function registerTtsAction() {
+	const ttsAction = new FileAction({
+		id: 'assistant-tts',
+		parent: 'assistant-group',
+		displayName: (nodes) => {
+			return t('approval', 'Text to speech')
+		},
+		enabled(nodes, view) {
+			return !actionIgnoreLists.includes(view.id)
+				&& nodes.length === 1
+				&& !nodes.some(({ permissions }) => (permissions & Permission.READ) === 0)
+				&& nodes.every(({ type }) => type === FileType.File)
+				&& nodes.every(({ mime }) => ['text/plain', 'text/markdown'].includes(mime))
+		},
+		iconSvgInline: () => MicrophoneMessageSvgIcon,
+		order: 0,
+		async exec(node) {
+			const { default: axios } = await import('@nextcloud/axios')
+			const { generateOcsUrl } = await import('@nextcloud/router')
+			const { showError, showSuccess } = await import('@nextcloud/dialogs')
+			const url = generateOcsUrl('/apps/assistant/api/v1/file-action/{fileId}/core:text2speech', { fileId: node.fileid })
+			try {
+				const response = await axios.post(url)
+				console.debug('taskId', response.data.ocs.data.taskId)
+				showSuccess(t('assistant', 'The result file will be created soon'))
+			} catch (error) {
+				console.error(error)
+				showError(t('assistant', 'Failed to launch the AI file action'))
+			}
+			return null
+		},
+	})
+	registerFileAction(ttsAction)
+}
+
+function registerSttAction() {
+	const sttAction = new FileAction({
+		id: 'assistant-stt',
+		parent: 'assistant-group',
+		displayName: (nodes) => {
+			return t('approval', 'Transcribe audio')
+		},
+		enabled(nodes, view) {
+			return !actionIgnoreLists.includes(view.id)
+				&& nodes.length === 1
+				&& !nodes.some(({ permissions }) => (permissions & Permission.READ) === 0)
+				&& nodes.every(({ type }) => type === FileType.File)
+				&& nodes.every(({ mime }) => ['audio/mpeg', 'audio/wav', 'audio/mp3'].includes(mime))
+		},
+		iconSvgInline: () => AccountVoiceSvgIcon,
+		order: 0,
+		async exec(node) {
+			const { default: axios } = await import('@nextcloud/axios')
+			const { generateOcsUrl } = await import('@nextcloud/router')
+			const { showError, showSuccess } = await import('@nextcloud/dialogs')
+			const url = generateOcsUrl('/apps/assistant/api/v1/file-action/{fileId}/core:audio2text', { fileId: node.fileid })
+			try {
+				const response = await axios.post(url)
+				console.debug('taskId', response.data.ocs.data.taskId)
+				showSuccess(t('assistant', 'The result file will be created soon'))
+			} catch (error) {
+				console.error(error)
+				showError(t('assistant', 'Failed to launch the AI file action'))
+			}
+			return null
+		},
+	})
+	registerFileAction(sttAction)
+}
+
+const assistantEnabled = loadState('assistant', 'assistant-enabled', false)
+const summarizeAvailable = loadState('assistant', 'summarize-available', false)
+const sttAvailable = loadState('assistant', 'stt-available', false)
+const ttsAvailable = loadState('assistant', 'tts-available', false)
+
+if (assistantEnabled) {
+	if (summarizeAvailable || sttAvailable || ttsAvailable) {
+		registerGroupAction()
+	}
+	if (sttAvailable) {
+		registerSttAction()
+	}
+	if (summarizeAvailable) {
+		registerSummarizeAction()
+	}
+	if (ttsAvailable) {
+		registerTtsAction()
+	}
+}
