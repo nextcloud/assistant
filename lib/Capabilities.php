54e46224835cb4548dfe4d58a0c9c8b93abe706e
Julien Veyssier <julien-nc@posteo.net>
2025-08-26 13:49:07 +0200

    feat(declarativeui): add hooks for each of our file actions
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Capabilities.php
+++ lib/Capabilities.php
@@ -14,6 +14,11 @@ use OCP\App\IAppManager;
 use OCP\Capabilities\IPublicCapability;
 use OCP\IAppConfig;
 use OCP\IConfig;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+use OCP\TaskProcessing\IManager;
+use OCP\TaskProcessing\TaskTypes\AudioToText;
+use OCP\TaskProcessing\TaskTypes\TextToTextSummary;
 
 class Capabilities implements IPublicCapability {
 
@@ -21,6 +26,9 @@ class Capabilities implements IPublicCapability {
 		private IAppManager $appManager,
 		private IConfig $config,
 		private IAppConfig $appConfig,
+		private IManager $taskProcessingManager,
+		private IL10N $l,
+		private IUrlGenerator $urlGenerator,
 		private ?string $userId,
 	) {
 	}
@@ -30,22 +38,105 @@ class Capabilities implements IPublicCapability {
 	 *     assistant: array{
 	 *         version: string,
 	 *         enabled?: bool
-	 *     }
+	 *     },
+	 *     declarativeui?: array{
+	 *         hooks: list<array{
+	 *             type: 'context-menu',
+	 *             endpoints: list<array{
+	 *                 name: string,
+	 *                 url: string,
+	 *                 filter: string,
+	 *                 android_icon: string,
+	 *                 desktop_icon: string,
+	 *                 ios_icon: string,
+	 *             }>
+	 *         }>
+	 *	   },
 	 * }
 	 */
 	public function getCapabilities(): array {
+		// App version
 		$appVersion = $this->appManager->getAppVersion(Application::APP_ID);
-		$capability = [
+		$capabilities = [
 			Application::APP_ID => [
 				'version' => $appVersion,
 			],
 		];
-		if ($this->userId !== null) {
-			$adminAssistantEnabled = $this->appConfig->getValueString(Application::APP_ID, 'assistant_enabled', '1') === '1';
-			$userAssistantEnabled = $this->config->getUserValue($this->userId, Application::APP_ID, 'assistant_enabled', '1') === '1';
-			$assistantEnabled = $adminAssistantEnabled && $userAssistantEnabled;
-			$capability[Application::APP_ID]['enabled'] = $assistantEnabled;
+		if ($this->userId === null) {
+			return $capabilities;
 		}
-		return $capability;
+
+		$adminAssistantEnabled = $this->appConfig->getValueString(Application::APP_ID, 'assistant_enabled', '1') === '1';
+		$userAssistantEnabled = $this->config->getUserValue($this->userId, Application::APP_ID, 'assistant_enabled', '1') === '1';
+		$assistantEnabled = $adminAssistantEnabled && $userAssistantEnabled;
+		$capabilities[Application::APP_ID]['enabled'] = $assistantEnabled;
+
+		// declarative UI
+		$availableTaskTypes = $this->taskProcessingManager->getAvailableTaskTypes();
+		$summarizeAvailable = array_key_exists(TextToTextSummary::ID, $availableTaskTypes);
+		$sttAvailable = array_key_exists(AudioToText::ID, $availableTaskTypes);
+		$ttsAvailable = class_exists('OCP\\TaskProcessing\\TaskTypes\\TextToSpeech')
+			&& array_key_exists(\OCP\TaskProcessing\TaskTypes\TextToSpeech::ID, $availableTaskTypes);
+
+		if ($summarizeAvailable || $sttAvailable || $ttsAvailable) {
+			$capabilities['declarativeui'] = [
+				'hooks' => [
+					[
+						'type' => 'context-menu',
+						'endpoints' => [],
+					],
+				],
+			];
+
+			if ($summarizeAvailable) {
+				$endpoint = [
+					'name' => $this->l->t('Summarize'),
+					'url' => $this->urlGenerator->linkToOCSRouteAbsolute(Application::APP_ID . '.assistantApi.runFileAction', [
+						'apiVersion' => 'v1',
+						'fileId' => '{s}',
+						'taskTypeId' => TextToTextSummary::ID,
+					]),
+					'filter' => 'text/',
+					'android_icon' => 'creation',
+					'ios_icon' => 'creation',
+					'desktop_icon' => 'creation',
+				];
+				$capabilities['declarativeui']['hooks'][0]['endpoints'][] = $endpoint;
+			}
+
+			if ($sttAvailable) {
+				$endpoint = [
+					'name' => $this->l->t('Transcribe audio'),
+					'url' => $this->urlGenerator->linkToOCSRouteAbsolute(Application::APP_ID . '.assistantApi.runFileAction', [
+						'apiVersion' => 'v1',
+						'fileId' => '{s}',
+						'taskTypeId' => \OCP\TaskProcessing\TaskTypes\TextToSpeech::ID,
+					]),
+					'filter' => 'audio/',
+					'android_icon' => 'speech_to_text',
+					'ios_icon' => 'speech_to_text',
+					'desktop_icon' => 'speech_to_text',
+				];
+				$capabilities['declarativeui']['hooks'][0]['endpoints'][] = $endpoint;
+			}
+
+			if ($ttsAvailable) {
+				$endpoint = [
+					'name' => $this->l->t('Text to speech'),
+					'url' => $this->urlGenerator->linkToOCSRouteAbsolute(Application::APP_ID . '.assistantApi.runFileAction', [
+						'apiVersion' => 'v1',
+						'fileId' => '{s}',
+						'taskTypeId' => AudioToText::ID,
+					]),
+					'filter' => 'text/',
+					'android_icon' => 'text_to_speech',
+					'ios_icon' => 'text_to_speech',
+					'desktop_icon' => 'text_to_speech',
+				];
+				$capabilities['declarativeui']['hooks'][0]['endpoints'][] = $endpoint;
+			}
+		}
+
+		return $capabilities;
 	}
 }
