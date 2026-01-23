198bc13f20e7ca2661c8c2ffb660fe850dac8f03
Jana Peper <jana.peper@nextcloud.com>
2026-01-23 14:43:36 +0100

    fix: update outdated summaries every 10 min
    Signed-off-by: Jana Peper <jana.peper@nextcloud.com>

--- lib/BackgroundJob/RegenerateOutdatedChatSummariesJob.php
+++ lib/BackgroundJob/RegenerateOutdatedChatSummariesJob.php
@@ -20,7 +20,7 @@ class RegenerateOutdatedChatSummariesJob extends TimedJob {
 		private SessionSummaryService $sessionSummaryService,
 	) {
 		parent::__construct($timeFactory);
-		$this->setInterval(60 * 60 * 24); // 24h
+		$this->setInterval(60 * 10); // 10min
 	}
 	public function run($argument) {
 		$userId = $argument['userId'];
