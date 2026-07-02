df2411000cfb46a26c568d402d730c3ae4955514
Marcel Klehr <mklehr@gmx.net>
2026-07-02 15:30:11 +0200

    fix: Correct code comment syntax
    Co-authored-by: Julien Veyssier <julien-nc@posteo.net>
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- src/components/icons/aliases.js
+++ src/components/icons/aliases.js
@@ -1,7 +1,7 @@
-<!--
-  - SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
-  - SPDX-License-Identifier: AGPL-3.0-or-later
--->
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import ToolInformationIcon from 'vue-material-design-icons/InformationBox.vue'
 import ReasoningContentIcon from 'vue-material-design-icons/ThoughtBubbleOutline.vue'
 
